f9dafc7b0009467a44a00cc6087c802dcd25d7df
magali rodriguez <magali.rodriguez@alu.inspt.utn.edu.ar>
2024-04-18 17:20:38 -0300

    calculadora v3.4

--- sumador.c
+++ sumador.c
@@ -4,13 +4,20 @@
 
 int main(void)
 {
-int num1, num2, num3, suma; 
+int num1, num2, num3, suma, resta, multiplicacion, division;
 p("ingrese primer valor: ");
 s("%d", &num1);
 p ("ingrese segundo valor: ");
 s ("%d", &num2);
-p ("ingrese tercer valor");
+p ("ingrese tercer valor: ");
+s ("%d", &num3);
 suma = num1 + num2 + num3;
-p("%d + %d + %d = %d", num1,num2,num3,suma);
+p("%d + %d + %d = %d\n", num1,num2,num3,suma);
+resta = num1 - num2 - num3;
+p("%d - %d - %d = %d\n" , num1,num2,num3,resta);
+multiplicacion = num1 * num2 * num3;
+p("%d * %d * %d = %d\n", num1,num2,num3,multiplicacion);
+division = num1 / num2 / num3;
+p("%d / %d / %d = %d", num1,num2,num3,division);
 return 0;
 }
